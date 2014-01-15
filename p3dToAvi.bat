080311683d34060f8c28b37b68251b2cb7b4a708
Daniel Clarke <dan137clarke@gmail.com>
2014-01-15 11:04:56 +1000

    created batch file to generate avi from proof3d

---
+++ p3dToAvi.bat
@@ -0,0 +1,11 @@
+@echo off
+call set PARENT_DIR=%CD%
+set PARENT_DIR=%PARENT_DIR:\= %
+set LAST_WORD=
+for %%i in (%PARENT_DIR%) do set LAST_WORD=%%i
+
+call set LAYOUT_FILE=%LAST_WORD%.lay
+call set TRACE_FILE=%LAST_WORD%.atf
+call set AVI_FILE=%LAST_WORD%.avi
+
+start C:\Wolverine\P3D\sp3d.exe /MakeAVI 800 480 0 10 %LAYOUT_FILE% %TRACE_FILE% %AVI_FILE%
