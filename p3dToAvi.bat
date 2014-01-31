67333abe6ac367f16d6639dbb182c95bcf6ec6e6
Daniel Clarke <dan137clarke@gmail.com>
2014-01-31 14:04:09 +1000

    slx is now parallelised; removed unused processors from layout file

--- p3dToAvi.bat
+++ p3dToAvi.bat
@@ -1,11 +1,1 @@
-@echo off
-call set PARENT_DIR=%CD%
-set PARENT_DIR=%PARENT_DIR:\= %
-set LAST_WORD=
-for %%i in (%PARENT_DIR%) do set LAST_WORD=%%i
-
-call set LAYOUT_FILE=%LAST_WORD%.lay
-call set TRACE_FILE=%LAST_WORD%.atf
-call set AVI_FILE=%LAST_WORD%.avi
-
-start C:\Wolverine\P3D\sp3d.exe /MakeAVI 800 480 0 180 %LAYOUT_FILE% %TRACE_FILE% %AVI_FILE%
+start C:\Wolverine\P3D\sp3d.exe /MakeAVI 800 480 0 180 airport.lay data_set_z\airport.atf data_set_z\airport.avi
