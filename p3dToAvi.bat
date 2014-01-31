713e41caec22eb7ed8524a250649d646f45b27f4
Daniel Clarke <dan137clarke@gmail.com>
2014-01-31 16:56:31 +1000

    extended the final queue length and moved the camera

--- p3dToAvi.bat
+++ p3dToAvi.bat
@@ -1,1 +1,1 @@
-start C:\Wolverine\P3D\sp3d.exe /MakeAVI 800 480 0 180 airport.lay data_set_z\airport.atf data_set_z\airport.avi
+start C:\Wolverine\P3D\sp3d.exe /MakeAVI 1280 720 0 180 airport.lay data_set_z\airport.atf data_set_z\airport.avi
